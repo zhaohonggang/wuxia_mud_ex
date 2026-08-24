fd1c1c1ca256b2ad9deb4b52869880f43764f7aa
honggang <zhaohonggang@yahoo.com>
2026-08-23 22:30:03 -0400

    Show hidden attributes in info command output

--- lib/kantele/character/commands/info_command.ex
+++ lib/kantele/character/commands/info_command.ex
@@ -4,6 +4,18 @@ defmodule Kantele.Character.InfoCommand do
   alias Kantele.Character.InfoView
 
   def run(conn, _params) do
-    render(conn, InfoView, "display")
+    character = conn.character
+    stats = character.meta.stats
+
+    conn
+    |> render(InfoView, "display", %{
+      vitals: character.meta.vitals,
+      str: stats.str,
+      dex: stats.dex,
+      con: stats.con,
+      int: stats.int,
+      combat_exp: stats.combat_exp,
+      potential: stats.potential
+    })
   end
 end
--- lib/kantele/character/views/info_view.ex
+++ lib/kantele/character/views/info_view.ex
@@ -3,16 +3,16 @@ defmodule Kantele.Character.InfoView do
 
   alias Kalevala.Character.Conn.EventText
 
-  def render("display", %{character: character}) do
+  def render("display", assigns) do
     %EventText{
       topic: "Character.Info",
-      data: character,
-      text: render("_display", %{character: character})
+      data: assigns,
+      text: render("_display", assigns)
     }
   end
 
-  def render("_display", %{character: character}) do
-    vitals = character.meta.vitals
+  def render("_display", assigns) do
+    vitals = assigns.vitals
 
     ~i"""
     {table}
@@ -27,6 +27,26 @@ defmodule Kantele.Character.InfoView do
       {row}
         {cell}内力{/cell}
         {cell}{ep}#{vitals.neili}/#{vitals.max_neili}{/ep}{/cell}
+      {/row}
+    {/table}
+    {table}
+      {row}
+        {cell}膂力{/cell}
+        {cell}#{assigns.str}{/cell}
+        {cell}身法{/cell}
+        {cell}#{assigns.dex}{/cell}
+      {/row}
+      {row}
+        {cell}根骨{/cell}
+        {cell}#{assigns.con}{/cell}
+        {cell}悟性{/cell}
+        {cell}#{assigns.int}{/cell}
+      {/row}
+      {row}
+        {cell}实战经验{/cell}
+        {cell}#{assigns.combat_exp}{/cell}
+        {cell}潜能{/cell}
+        {cell}#{assigns.potential}{/cell}
       {/row}
     {/table}
     """
