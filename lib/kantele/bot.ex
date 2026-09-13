c126bade7aaa84d7a16850b88ebb589ecf5ea8e3
honggang <zhaohonggang@yahoo.com>
2026-09-12 23:11:02 -0400

    fix: bot.ex map update syntax - fix leading comma syntax

--- lib/kantele/bot.ex
+++ lib/kantele/bot.ex
@@ -532,7 +532,7 @@ defp decide(st, cfg, state) do
       learn_cmds = Enum.filter(state.config.train, &is_learn_cmd/1)
       all_failed = Enum.all?(learn_cmds, &MapSet.member?(failed, &1))
       
-new_state = %{state 
+new_state = %{state
          | pending_learn: nil
          , failed_train_commands: failed
          , train_index: 0
